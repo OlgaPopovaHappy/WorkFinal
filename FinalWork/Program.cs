591c30fe4bbb68fd8fdb0d5ca2c5f80a383e1f28
OlgaPopova <109219990+OlgaPopovaHappy@users.noreply.github.com>
2022-09-18 11:59:32 +0300

    Создана папка FinalWork

---
+++ FinalWork/Program.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Задайте массив строк через пробел: ");
+string[] array = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+string[] newArray = new string[array.Length];
+SelectFromArray(array, newArray);
+PrintArray(newArray);
+
+void SelectFromArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Write($"{array[i]} ");
+    }
+}
